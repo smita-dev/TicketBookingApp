482c8a9d9a5a3f2fa11db6d77237baaeea1e2996
smita <smitakpatil26@gmail.com>
2024-06-08 19:16:53 +0530

    create entities

---
+++ app/src/main/java/ticket.booking/entities/User.java
@@ -0,0 +1,11 @@
+package ticket.booking.entities;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private String userId;
+    private String password;
+    private String hashPassword;
+    private List<Ticket> bookedTicket;
+}
